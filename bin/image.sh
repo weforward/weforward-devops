246af01ac8dca0e98397853c93476096d4fd927c
bossky <bossky8023@gmail.com>
2021-06-13 11:50:09 +0800

    增加bin脚本

---
+++ bin/image.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NAME=${0%/*}
+NAME=${NAME%/*}
+NAME=${NAME##*/}
+
+IMAGE_NAME=$NAME
+
+VERSION=`docker exec $NAME java VersionUtil $NAME.jar`
+
+if [ $? -ne 0 ] ; then 
+	echo 'VERSION ERROR'
+	exit 1
+fi
+
+echo "Version "$VERSION
+
+if [ $# -lt  2 ] ; then
+	read -p "Input Docker User:" DOCKERUSER
+	echo $DOCKERUSER
+	read -s -p "Input Docker Password:" DOCKERPASSWORD
+else
+	DOCKERUSER=$1
+	DOCKERPASSWORD=$2
+fi 
+
+DOCKER_HUB=git.navboy.com:5000
+
+docker tag $IMAGE_NAME $DOCKER_HUB/$IMAGE_NAME:$VERSION
+
+docker login -u $DOCKERUSER -p $DOCKERPASSWORD $DOCKER_HUB 
+
+if [ $? -ne 0 ] ; then 
+	echo 'User or Password ERROR'
+	exit 1
+fi
+
+docker push $DOCKER_HUB/$IMAGE_NAME:$VERSION
+
+docker logout $DOCKER_HUB
+if [ $? -ne 0 ] ; then 
+	echo "上传$DOCKER_HUB/$IMAGE_NAME:$VERSION失败"
+else
+	echo "成功上传$DOCKER_HUB/$IMAGE_NAME:$VERSION"
+fi
+
+  
---
+++ bin/upgrade.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+NAME=${0%/*}
+NAME=${NAME%/*}
+NAME=${NAME##*/}
+
+IMAGE_NAME=$NAME
+
+cd `dirname $0`;
+
+CONF_DIR=/wf/ms/$NAME/conf
+
+SCRIPT_DIR=/wf/ms/$NAME/script
+
+SITE_DIR=/wf/ms/$NAME/site
+
+LOG_DIR=/var/log/$NAME
+
+CA_DIR=/wf/ca/
+
+DATA_DIR=/data/
+
+docker build . -t $IMAGE_NAME
+
+docker stop $NAME-old
+
+docker rm $NAME-old
+
+docker stop $NAME
+
+docker rename $NAME $NAME-old
+
+V_OPT=""
+
+if [ -d $CONF_DIR ]; then
+	V_OPT=" -v $CONF_DIR:/home/boot/conf/"
+fi
+
+if [ -d $SCRIPT_DIR ]; then
+	V_OPT=$V_OPT" -v $SCRIPT_DIR:/home/boot/script/"
+fi
+
+if [ -d $CA_DIR ]; then
+	V_OPT=$V_OPT" -v $CA_DIR:/home/boot/ca/"
+fi
+
+if [ -d $SITE_DIR ]; then
+	V_OPT=$V_OPT" -v $SITE_DIR:/home/boot/site/"
+fi
+
+if [ -d $LOG_DIR ]; then
+	V_OPT=$V_OPT" -v $LOG_DIR:/home/boot/log/"
+fi
+
+if [ -d $DATA_DIR ]; then
+	V_OPT=$V_OPT" -v $DATA_DIR:/home/boot/data/"
+fi
+
+EVN_OPT=""
+
+if [ -f ./my.env ]; then
+   EVN_OPT="--env-file ./my.env"
+fi
+
+VERSION=`java VersionUtil $NAME.jar`
+
+docker run --restart=unless-stopped -d $V_OPT -v /etc/localtime:/etc/localtime:ro --env PROJECT_VERSION=$VERSION $EVN_OPT --net host --name $NAME $IMAGE_NAME
+
+exit 0
